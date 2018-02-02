2f23a187716c9fe99cf03290aec5523f80823b91
ritato <sabaliauskaite.r@gmail.com>
2018-02-02 21:27:09 +0200

    first commit

--- src/ScannerExample.java
+++ src/ScannerExample.java
@@ -4,32 +4,36 @@ public class ScannerExample {
 
     public static void main(String[] args) {
 
-        System.out.println("Iveskite savo varda");
-
         Scanner sc = new Scanner(System.in); //vartotojo irasytos info nuskaitymas
+        ScannerExample scannerExample=new ScannerExample();
+        String name="";
+        do {
+
+            System.out.println("Iveskite savo varda");
+            name = sc.nextLine(); // nuskaito visa eilute
+            System.out.println("Jusu vardas yra " + name);
 
 
-        String name = sc.nextLine(); // nuskaito visa eilute
+            String a = "Petras";
+            String b = "Petras";
+
+            if (a.equals(b)) { //stringus galima patikrinti ar jie lygus tik su equal
+                System.out.println("LYGUS " + a);
+            } else {
+                System.out.println("NE");
+            }  // ScannerExample sca
+            int length = name.length(); //kodo sutrumpinimas, daznai kartojasi name.lenght, todel susikuriam kintamaji
+            if (length % 2 == 0) {
+                System.out.println("Ivestas zodis " + name + " yra lyginis, ilgis " + length);
+            } else {
+                System.out.println("Ivestas zodis yra " + name + " yra nelyginis, ilgis " + length + ", rasta raidziu" );
+            }
+
+
+        } while (!name.equals("pabaiga"));
+
+
         //string name=sc.next();// nuskaito teksta iki tarpo
-
-        System.out.println("Jusu vardas yra " + name);
-
-
-        String a = "Petras";
-        String b = "Petras";
-
-        if (a.equals(b)) { //stringus galima patikrinti ar jie lygus tik su equal
-            System.out.println("LYGUS " + a);
-        } else {
-            System.out.println("NE");
-        }  // ScannerExample sca
-        int length = name.length(); //kodo sutrumpinimas, daznai kartojasi name.lenght, todel susikuriam kintamaji
-        if (length % 2 == 0) {
-            System.out.println("Ivestas zodis " + name + " yra lyginis, ilgis " + length);
-        } else {
-            System.out.println("Ivestas zodis yra " + name + "yra nelyginis, ilgis " + length);
-        }
-
 
     }
 
